2dc0df29f1186f802b688ad479db830ddbb2d63e
Kade Chambers <kgbchambers@gmail.com>
2020-09-21 13:27:18 -0700

    fixed kindaBuggy bugs

--- main.cpp
+++ main.cpp
@@ -1,24 +1,19 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
-int main()
-{
-	string name;
-	bool age;
-
-	cout<<"Hi, what's your name?";
-	getline(cin,name);
-
-	cout<<name<<,<<what is your age?;
-	cin>>age>>endl;
-
-	if( age >0 && age < 12 )
-		cout<<"Hey kid!"<<endl;
-	if( age < 18 )
-		cout<<"I hope you're enjoying highschool!"<<endl;
-	else
-		cout<<"Oh, you're an adult. Nice to meet you."<<endl;
-
-	return 0;
+int main(){
+        string name;
+        int age;
+        cout<<"Hi, what's your name?";
+        getline(cin,name);
+        cout<<name<<","<<"what is your age?";
+        cin>>age;
+        if( age > 0 && age < 12 )
+                cout<<"Hey kid!"<<endl;
+        else if( age < 18 )
+                cout<<"I hope you're enjoying highschool!"<<endl;
+        else
+                cout<<"Oh, you're an adult. Nice to meet you."<<endl;
+        return 0;
 }
